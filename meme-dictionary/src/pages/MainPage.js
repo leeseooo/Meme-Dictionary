ddf363ece34bbda59ff5179a9a7d2dab22acdbff
leeseooo <leeseoooo@naver.com>
2022-04-01 19:25:57 +0900

    [ADD]메인페이지 두번째 컨테이너 생성

--- meme-dictionary/src/pages/MainPage.js
+++ meme-dictionary/src/pages/MainPage.js
@@ -24,20 +24,20 @@ export default function MainPage() {
 
                     <p>
                         사용자들에게 가장
-                        인기있는 단어 밈 순위입니다.
-                        업데이트를 클릭하면
-                        실시간 좋아요 수에 따라
+                        인기있는 단어<br /> 밈 순위입니다.
+                        업데이트 버튼을 클릭하면<br />
+                        실시간 좋아요 수에 따라<br />
                         순위가 변경됩니다.
                     </p>
                 </div>
                 <div className={styles.rightSide}>
                     <button>업데이트</button>
-                    <ul className={styles.rankingList}>
-                        <li>어쩔티비 저쩔티비 시크릿쥬쥬 삼성티비</li>
-                        <li>신기방기 뿡뿡방기</li>
-                        <li>알잘딱깔센</li>
-                        <li>그냥 꼬옥 안아주면 되</li>
-                        <li>롬곡옾눞</li>
+                    <ul>
+                        <div style={{ display: 'flex' }}><span style={{ paddingRight: '80px', color: '#fff', fontSize: '30px' }}>1</span><li>어쩔티비 저쩔티비 시크릿쥬쥬 삼성티비 어쩔티비 저쩔티비 시크릿쥬쥬 삼성티비</li></div>
+                        <div style={{ display: 'flex' }}><span style={{ paddingRight: '80px', color: '#fff', fontSize: '30px' }}>2</span><li>신기방기 뿡뿡방기 청기백기 동방신기</li></div>
+                        <div style={{ display: 'flex' }}><span style={{ paddingRight: '80px', color: '#fff', fontSize: '30px' }}>3</span><li>알잘딱깔센</li></div>
+                        <div style={{ display: 'flex' }}><span style={{ paddingRight: '80px', color: '#fff', fontSize: '30px' }}>4</span><li>그냥 꼬옥 안아주면 되</li></div>
+                        <div style={{ display: 'flex' }}><span style={{ paddingRight: '80px', color: '#fff', fontSize: '30px' }}>5</span><li>롬곡옾눞</li></div>
                     </ul>
                 </div>
             </div>
